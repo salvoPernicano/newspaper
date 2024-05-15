a4f8109a3b5972697871d7340a4275785d8bbc0d
salvoPernicano <salvo.pernicano@gmail.com>
2024-05-15 12:41:47 +0200

    fix comments

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// Dipendenze
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+//configurazione server
 const app = express();
 const PORT = 8000;
 const url = 'https://tg24.sky.it/';
 
+//montaggio middleware per evitare errori cors
 app.use(cors());
 
+//recupero dati
 app.get('/articles', async (req, res) => {
   try {
 
@@ -18,6 +22,7 @@ app.get('/articles', async (req, res) => {
 
     const articles = [];
 
+//Per ogni tag H2 con classe c-card__title recuperiamo il testo al suo interno
     $('h2.c-card__title').each(function() {
       const title = $(this).text();
       articles.push({ title});
@@ -25,14 +30,17 @@ app.get('/articles', async (req, res) => {
 
     const links = [];
 
+//Per ogni tag <a> con classe c-card recuperiamo l'attributo HREF
     $('a.c-card').each(function(){
       const link = $(this).attr('href');
+
+      //Contorlliamo che non abbia valore NULL
       if(link){
         links.push(link)
       }
     })
-    console.log(articles.length)
-    console.log(links.length)
+    
+    //cicliamo l'array dei titoli e sfruttiamo l'index per assegnare i link corrispondenti
     const articlesWithLinks = articles.map((article,index) => {
       return {
         title: article.title,
